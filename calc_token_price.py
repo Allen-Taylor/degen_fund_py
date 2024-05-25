406f5cc04266b2279bedbb957c452ceb6ee8d7ef
AL THE BOT FATHER <98119917+Allen-Taylor@users.noreply.github.com>
2024-05-25 22:56:25 +0200

    Delete calc_token_price.py

--- calc_token_price.py
+++
@@ -1,9 +0,0 @@
-def calc_token_price(token_reserves, sol_price=117.20):
-    a = 2.17e-22
-    b = -4.04e-13
-    c = 0.0002283
-    
-    token_price_in_sol = a * token_reserves**2 + b * token_reserves + c
-    token_price_in_usd = token_price_in_sol * sol_price
-    
-    return token_price_in_usd
